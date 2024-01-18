d788942a8dd95c8f3569c6ab2c01b0c7a19535ed
Guilherme Vilela <guivilela700@gmail.com>
2024-01-17 23:29:52 -0300

    Add GUI
    Added a simple GUI that can start, stop and close the program. Gotta make it prettier.

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from mss import mss
 import mss.tools
 import keyboard
 from pytesseract import pytesseract
-from PIL import Image
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 import difflib
 import cv2 as cv
+import PySimpleGUI as psg
+import time
 
 
 path_to_tesseract = "./Tesseract/tesseract.exe"
@@ -17,6 +18,10 @@ all_quotes = ["Some weather we're having, huh?", "Hey hivekin, can I bug you for
               "Me-wow, is that the latest Felinor fashion?", "Wow, this breeze is great, right?",
               "You ever been to a Canor restaurant? The food's pretty howlright.",
               "So, what's keeping you busy these days?"]
+
+# Providing the tesseract executable
+# location to pytesseract library
+pytesseract.tesseract_cmd = path_to_tesseract
 
 
 def get_area_primary_monitor():
@@ -38,8 +43,8 @@ def take_screenshot(top, left):
 
         # Save to the picture file
         mss.tools.to_png(sct_img.rgb, sct_img.size, output=output)
-        print(output)
         return output
+
 
 def grayscale(imgPath):
     image = cv.imread(imgPath)
@@ -48,38 +53,43 @@ def grayscale(imgPath):
     return grayIMG
 
 
+layout = [[psg.Button("Start")], [psg.Button(button_text="Stop", visible=False)], [psg.Button("Close")]]
 
-area = get_area_primary_monitor()
-print(area)
-image_path = take_screenshot(int(area[0]), int(area[1]))
-# image_path = "sct-442x768_400x35.png"
+window = psg.Window("Auto Charisma", layout, resizable=True)
 
-print(image_path)
+on = False
 
+while True:
+    if on:
+        image_path = take_screenshot(int(area[0]), int(area[1]))
+        img = grayscale(image_path)
+        ret, img = cv.threshold(img, 204, 255, cv.THRESH_BINARY_INV)
 
+        text = pytesseract.image_to_string(img, config="-c tessedit_char_whitelist=' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ?-,'")
 
-img = grayscale(image_path)
+        fuzz.SequenceMatcher = difflib.SequenceMatcher
+        result = process.extractOne(text, all_quotes)
 
-ret, img = cv.threshold(img, 204, 255, cv.THRESH_BINARY_INV)
+        if result[1] > 80:
+            keyboard.write(result[0])
+            time.sleep(1)
+            keyboard.press("enter")
 
-# cv.imshow('THRESHOLH IMAGE', img)
-# cv.waitKey(0)
-# cv.destroyAllWindows()
+    event, values = window.read(timeout=1000)
+    if event == psg.WIN_CLOSED or event == "Close":
+        window.close()
+        break
 
-# Providing the tesseract executable
-# location to pytesseract library
-pytesseract.tesseract_cmd = path_to_tesseract
+    if event == "Start":
+        window["Start"].update(visible=False)
+        window["Stop"].update(visible=True)
+        on = True
+        area = get_area_primary_monitor()
 
-# Passing the image object to image_to_string() function
-# This function will extract the text from the image
-text = pytesseract.image_to_string(img, config="-c tessedit_char_whitelist=' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ?-,'")
+    if event == "Stop":
+        window["Stop"].update(visible=False)
+        window["Start"].update(visible=True)
+        on = False
 
-# Displaying the extracted text
-print(text)
+window.close()
 
-fuzz.SequenceMatcher = difflib.SequenceMatcher
-result = process.extractOne(text, all_quotes)
-print(result)
-if result[1] > 80:
-    keyboard.write(result[0])
-    keyboard.press("enter")
